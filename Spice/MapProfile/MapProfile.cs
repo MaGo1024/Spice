bfaef29c230356223f2d117a09fc29c0ec08863d
MaGo1024 <34481013+MaGo1024@users.noreply.github.com>
2020-10-22 16:29:59 +0100

    Add project files.

---
+++ Spice/MapProfile/MapProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Spice.Models;
+using Spice.Models.ViewModels;
+
+namespace Spice.MapProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<SubCategoryAndCategoryViewModel, Category>()
+                .ForMember(vm => vm.Name, s => s.MapFrom(source => source.SubCategory.Name));
+        }
+    }
+}
